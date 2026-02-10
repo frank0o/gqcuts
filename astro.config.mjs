db60885c0593e2592d2409e7c9bd3e9689e20154
frank0o <warandpunishment@gmail.com>
2026-02-10 14:22:27 +1100

    doin

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,16 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
+import icon from 'astro-icon';
+
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  site:'https://frank0o.github.io',
+  base:'/gqcuts',
+  integrations: [icon({
+    include:{
+      bi: ['tiktok','instagram','facebook']
+    }
+  }),
+]
+});
---
+++ src/consts.ts
@@ -0,0 +1,12 @@
+export const SITE_DESCRIPTION = ""
+export const CONTACT_NUMBER = "+6110306101"
+export const ADDRESS = "Shop G1/19, Challis Street, Dickson, 2602, Canberra, Australian Capital Territory"
+export const ADDRESS_LINK = "https://maps.app.goo.gl/ddeYMVwna6Eudu7FA"
+
+
+export const SITE_SOCIALS : Array<{icon:string,url:string}> = [
+  {icon:'facebook',url:"https://www.facebook.com/profile.php?id=61574922394339"}  ,
+  {icon:"instagram",url:"https://www.instagram.com/gqcut2602"},
+  {icon:"tiktok",url:"https://www.tiktok.com/@gq.cut"}
+
+]
