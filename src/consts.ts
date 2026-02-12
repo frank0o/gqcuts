c9083e7e3c064903fdc43bdcf082d596065fb4ff
frank0o <warandpunishment@gmail.com>
2026-02-12 13:33:00 +1100

    updated text and css

--- src/consts.ts
+++ src/consts.ts
@@ -1,4 +1,8 @@
-export const SITE_DESCRIPTION = ""
+export const SITE_DESCRIPTION = `We are a walk in barbershop, open 7 days a week. 
+Providing quality customer service and haircuts from layers to fades! Located at 19 Challis at Dickson, Canberra 2602 .
+Prices start from $35.
+1h free parking at TransACT car park.`
+
 export const CONTACT_NUMBER = "+6110306101"
 export const ADDRESS = "Shop G1/19, Challis Street, Dickson, 2602, Canberra, Australian Capital Territory"
 export const ADDRESS_LINK = "https://maps.app.goo.gl/ddeYMVwna6Eudu7FA"
